0d66ed163e9dc636af8bd38788d4c78cbca6131e
Ajay Javiya <javiyaajay1987@gmail.com>
2020-05-13 16:08:15 +0530

    [ADD] new module : crm_lead_documents

---
+++ crm_lead_documents/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Lead Documents",
+    "summary": "Add documents management for crm lead.",
+    'category': 'Sales/CRM',
+    "author": "Druidoo",
+    "website": "https://www.druidoo.io/",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["crm", "documents"],
+    "data": [
+        "views/lead_views.xml",
+        "views/documents_document_views.xml",
+        "views/assets.xml",
+    ],
+    "installable": True,
+}

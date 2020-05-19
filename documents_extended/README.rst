ccfb56898abe2edbc6f7a4e6ed73fca5910cbfd8
Chetan <scrdhaduk@gmail.com>
2020-05-19 10:40:08 +0530

    [ADD] Added documents_extended module.

---
+++ documents_extended/README.rst
@@ -0,0 +1,31 @@
+==============================
+Extend search view of document
+==============================
+
+|
+|
+|
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* Druidoo
+
+Contributors
+~~~~~~~~~~~~
+
+* Druidoo (<https://www.druidoo.io>)
+
+Maintainers
+~~~~~~~~~~~
+
+.. |maintainer-ivantodorovich| image:: https://github.com/ivantodorovich.png?size=40px
+    :target: https://github.com/ivantodorovich
+    :alt: ivantodorovich
+
+Current maintainer:
+
+|maintainer-ivantodorovich|
---
+++ documents_extended/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+{
+    "name": "Document management extended",
+    "version": "13.0.1.0.0",
+    "category": "Operations/Documents",
+    "author": "Druidoo",
+    "website": "https://www.druidoo.io",
+    "license": "AGPL-3",
+    "depends": [
+        "documents",
+    ],
+    "data": [
+        "views/events_views.xml",
+    ],
+    "installable": True,
+}
